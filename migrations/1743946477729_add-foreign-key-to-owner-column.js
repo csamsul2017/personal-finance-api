5e59fa4a37ba497f16fb0902708b982a83c9cf1d
csamsul2017 <csamsul2017@gmail.com>
2025-04-07 09:38:15 +0700

    add foreign key to notes owner column

---
+++ migrations/1743946477729_add-foreign-key-to-owner-column.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.addConstraint('finance', 'fk_finance_owner_users', {
+    foreignKeys: {
+      columns: 'owner',
+      references: 'users(id)',
+      onDelete: 'CASCADE',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropConstraint('finance', 'fk_finance_owner_users');
+};
